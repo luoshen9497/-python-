405c64fc0582f1a48a9324bb37b5f870063913d9
qingcheng <fenyukuang@gmail.com>
2020-03-27 18:20:00 +0800

    python2

--- qingcheng.py
+++ qingcheng.py
@@ -52,15 +52,62 @@
 # print(b)
 # print(a)
 #注意：True=1 ，False=0
-xx = [0,False,1,True]
-a = xx.count(0)
+# xx = [0,False,1,True] 
+# a = xx.count(0)
+# print(a)
+
+# #对于元组或者数组操作时，注意下标不要超出范围--越界
+# xx = [0,1,2,3]
+# print(xx[9])    #会报错
+# """
+# 所有的方法都是小括号结尾，比如：print（），input（），lenI（）。
+# 元组,数组，字典的取值，都是用中括号[]，比如a[9]
+# 元组，数组，字典的定义，分别是：(),[]，{}
+
+"""
+字典的特点
+1.字典的值没有顺序
+2.字典的接口必须是键值对的结构。key:Value
+3.字典的取值，是通过key去取值value
+"""
+a = {"name":"洛洛",0:"xixi","age":99}
+#取值get
+print(a["name"])
+#新增 pop
+a["high"] = "183cm"
+print(a)
+#修改/update
+a["name"] = "青城"
 print(a)
 
-#对于元组或者数组操作时，注意下标不要超出范围--越界
-xx = [0,1,2,3]
-print(xx[9])    #会报错
+b = a.get("name")
+print(b)
+b = a.pop("name")
+print(b)
+
+a.update(name=9497)
+print(a)
+
+print("-----------------")
+print(a.get("name"))
+print(a["name"])
+print("-----------------")  #在字典中a.get和a["key"]效果相同，但取超出范围的值就不同
+# print(a.get("name99"))
+# print(a["name99"])
+
+#数组和字典的删除
+del a ["name"]
+print(a)
+
+del a[0]
 """
-所有的方法都是小括号结尾，比如：print（），input（），lenI（）。
-元组,数组，字典的取值，都是用中括号[]，比如a[9]
-元组，数组，字典的定义，分别是：(),[]，{}
+练习：
+获取用户输入的个人信息，并且存储到字典中
 """
+a = input("请输入name：")
+b = input("请输入age：")
+c = input("请输入high:")
+
+d = {"name:":a,"age":b,"high":c}
+print(d)
+
