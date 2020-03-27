939a3aa2e8683f4cc4892c2d46271b0330c64c60
qingcheng <fenyukuang@gmail.com>
2020-03-27 12:58:49 +0800

    第一次向Github提交代码

---
+++ new9497.py
@@ -0,0 +1,50 @@
+# # print("hello world！")  #字符串
+# # print(2333)  #整数
+# # print(2.33)  #小数
+# # print(True)  #布尔值  True False
+# # print(())    #元组
+# # print([])    #数组
+# # print({})    #字典 
+# # """
+# # 网恋选我我最甜
+# # 公开不绿加关联
+# # """
+# # print("青城",9497,13.14)
+# # print("好好学习"+"天天向上")
+# # print("我爱你"*99)
+# # print(((1+2)*100-9.9)/2)
+# # print(2>3)
+
+# # # 变量
+# # # 赋值
+# # name = "洛洛" # 把洛洛这个值赋值给了名字叫a的这个变量
+# # print(name)
+
+
+
+# # a = float(input("请输入:"))
+# # b = float(input("请输入:"))
+# # print("input获取的内容:",a+b)
+
+# # 数据格式的转换
+# print(type(9497))
+# print(type(94.97))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# # a = "想你想你想你想你想你想你噢想我"
+# # print(len(a))
+ 
+"""
+练习：通过代码获取两段内容，并计算他们的长度的和。
+"""
+# a = input("请输入：")
+# b = input("请输入：")
+# print("两段字符串的长度是：",len(a)+len(b))
+
+a = 123
+b = 456
+len(a)+len(b)
+
